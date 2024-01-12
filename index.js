e44320a81e2306b9bdc527f1756d1a35da74e02a
Traspo <nyaspo8@gmail.com>
2024-01-11 20:29:00 -0500

    working app!

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-// const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-inquirer
-  .prompt([
+// An array of questions for user input
+const questions = [
     {
         type: 'input',
         message: 'What is your project title?',
@@ -34,10 +33,9 @@ inquirer
     {
         type: 'list',
         message: 'Does this app have a license?',
-        choices: ['MIT', 'GNU GPLv3', 'Apache License 2.0', 'BSD 2-Clause License', 
-        'BSD 3-Clause License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal',
-         'Eclipse Public License 2.0', 'GNU AGPL v3', 'GNU GPL v2',
-          'GNU LGPL v3', 'Mozilla Public License 2.0', 'The Unlicense', 'N/A'], //ADD other licenses here
+        choices: ['MIT', 'GPLv3', 'Apache_2.0', 'BSD_2--Clause', 
+        'BSD_3--Clause', 'Boost_1.0', 'AGPL_v3', 'GPL_v2',
+          'LGPL_v3', 'MPL_2.0', 'Unlicense', 'N/A'],
         name: 'license',
     },
     {
@@ -47,7 +45,7 @@ inquirer
     },
     {
         type: 'input',
-        message: 'Are there tests that have been run on this?', //Need a different Q here
+        message: 'Are there tests that have been run on this?',
         name: 'tests',
     },
     {
@@ -60,73 +58,21 @@ inquirer
         message: 'Enter email address:',
         name: 'questionsEmail',
     },
-    ])
+    ]
 
-    .then(({projectTitle, description, installation, usage, credits, license, contributing, tests, questionsGitHub, questionsEmail}) => {
-        const readMeTemplate = `# ${projectTitle}
-        
-## Description
-    
-${description}
-
-## Table of Contents
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-- [Contribution](#contribution)
-- [Tests](#tests)
-- [Questions](#questions)
-
-## Installation
-
-${installation}
-
-## Usage
-
-${usage}
-
-## Credits
-
-Developer: ${credits}
-        
-## License
-
-${license}
-
-
-
-## Contribution
-
-${contributing}
-
-## Tests
-
-${tests}
-
-## Questions
-
-For Questions or comments, please contact:
-- GitHub: [${questionsGitHub}](https://github.com/${questionsGitHub})
-- Email: [${questionsEmail}](mailto:${questionsEmail})`;
-
-        //Run function to write the file
-        writeToFile(projectTitle, readMeTemplate);
-    }
-    );
-
-// TODO: Create a function to write README file
-function writeToFile(projectTitle, data) {
-    // fs.writeFile(`./appOutput/${title.toLowerCase().split(' ').join('')}README.md`, data, (err) =>
-    // err ? console.log(err) : console.log('Your README has been created!')
+// Function to write README file
+function writeToFile(data) {
     fs.writeFile(`./appOutput/README.md`, data, (err) =>
     err ? console.log(err) : console.log('Your README has been created!')
 );
 }
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        return writeToFile(generateMarkdown({...responses}));
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,53 +1,81 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// This function returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-
-// [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
-
-// [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)
-
-// [![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)
-
-// [![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)
-
-// [![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
-
-// [![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
-
-// [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-
-// [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
-
-// [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-
-// [![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
-
-// [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
-
-// [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+function renderLicenseBadge(license) {
+  if (license !== 'N/A') {
+    return `![License: ${license}](https://img.shields.io/badge/License-${license}-blue.svg)`
+  }
+  return '';
+}
 
 
-// TODO: Create a function that returns the license link
+// This function returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license){
+  if(license !== 'N/A') {
     return `https://opensource.org/licenses/${license}`
   } else {
     return ''
   }
 }
 
-// TODO: Create a function that returns the license section of README
+// This function returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'N/A') {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+Licensed under the ${license} license.
+For more information, please visit:`;
+  }
+}
+
+// This function generates the markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projectTitle}
+${renderLicenseBadge(data.license)}      
+## Description
+    
+${data.description}
 
-`;
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+Developer: ${data.credits}
+        
+${renderLicenseSection(data.license)}
+
+${renderLicenseLink(data.license)}
+
+## Contribution
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For Questions or comments, please contact:
+- GitHub: [${data.questionsGitHub}](https://github.com/${data.questionsGitHub})
+- Email: [${data.questionsEmail}](mailto:${data.questionsEmail})`;
 }
 
 module.exports = generateMarkdown;
